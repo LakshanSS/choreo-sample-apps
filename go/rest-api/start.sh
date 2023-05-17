f0dedf88393c0d2f22e2269878a6ae104ab9b30f
lakshanss <lakshan230897@gmail.com>
2023-05-17 16:33:44 +0530

    root

---
+++ go/rest-api/start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "aaaaaaaaaaaaaaaaaaaaa"
+/app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 & /app/tailscale up --authkey=tskey-auth-kQoi1j5CNTRL-sKh5MeZdMQYPgL8afzLDQYmC2tSUBBzwf
+echo Tailscale started
+ALL_PROXY=socks5://localhost:1055/ HTTP_PROXY=http://localhost:1055/ http_proxy=http://localhost:1055/ /go/bin/app
+
