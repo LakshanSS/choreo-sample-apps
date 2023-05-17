07fb3f1e629362ffe14575eac4db0a0e76a0ba01
lakshanss <lakshan230897@gmail.com>
2023-05-17 11:32:20 +0530

    hw

--- python/rest-api/app.py
+++ python/rest-api/app.py
@@ -12,14 +12,15 @@ def generate_id():
 # endpoint to get all books
 @app.route('/reading-list/books', methods=['GET'])
 def get_books():
-    url = "http://api.worldbank.org/v2//country/USA/indicator/SP.POP.TOTL?format=json"
+    # url = "http://api.worldbank.org/v2//country/USA/indicator/SP.POP.TOTL?format=json"
 
-    # A GET request to the API
-    response = requests.get(url)
+    # # A GET request to the API
+    # response = requests.get(url)
 
-    # Print the response
-    response_json = response.json()
-    print(response_json)
+    # # Print the response
+    # response_json = response.json()
+    # print(response_json)
+    print("Hello World")
     return jsonify({'books': books})
 
 # endpoint to add a new book
