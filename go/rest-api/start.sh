21cd7d62057e80f39097061083edec5eed965cbe
lakshanss <lakshan230897@gmail.com>
2023-05-22 21:05:21 +0530

    testSec

--- go/rest-api/start.sh
+++ go/rest-api/start.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-echo "before tailscaled"
-/app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 & /app/tailscale up --authkey=tskey-auth-kMSR6i2CNTRL-3sCc7yAmWTj9fHQ5SjUNTjXvkhpM8QMd
-echo "after tailscale up"
-ALL_PROXY=socks5://localhost:1055/ HTTP_PROXY=http://localhost:1055/ http_proxy=http://localhost:1055/ /go/bin/app
-echo "bbbbbbbbbbbbbbbbbbbbbb"
+echo "this is from start.sh"
+echo $TEST_SECRET
+echo "xxx"
+printenv TEST_SECRET
+# echo "before tailscaled"
+# /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 & /app/tailscale up --authkey=tskey-auth-kMSR6i2CNTRL-3sCc7yAmWTj9fHQ5SjUNTjXvkhpM8QMd
+# echo "after tailscale up"
+# ALL_PROXY=socks5://localhost:1055/ HTTP_PROXY=http://localhost:1055/ http_proxy=http://localhost:1055/ /go/bin/app
+# echo "bbbbbbbbbbbbbbbbbbbbbb"
 
 # --socks5-server=localhost:1055
 # ALL_PROXY=socks5://localhost:1055/
